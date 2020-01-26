7eb9689efc6268e603c1be8acadc27f270ccb2a0
Kir322 <Fedulov.Kirill17@gmail.com>
2020-01-26 19:38:50 +0200

    nord-theme

--- init.el
+++ init.el
@@ -10,6 +10,8 @@
 (global-visual-line-mode 1)
 (global-hl-line-mode 1)
 
+(load-theme 'nord t)
+
 (show-paren-mode 0)
 (tool-bar-mode 0)
 (scroll-bar-mode 0)
@@ -27,7 +29,21 @@
 (global-set-key (kbd "M-i") 'imenu)
 
 (custom-set-variables
- '(initial-frame-alist (quote ((fullscreen . maximized)))))
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("7f6d4aebcc44c264a64e714c3d9d1e903284305fd7e319e7cb73345a9994f5ef" default)))
+ '(initial-frame-alist (quote ((fullscreen . maximized))))
+ '(package-selected-packages (quote (nord-theme))))
 
 (add-to-list 'default-frame-alist '(font . "Monaco-14"))
 (set-face-attribute 'default t :font "Monaco-14")
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
