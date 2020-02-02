ee81c133e6fbaeefe6635db4f3d3ad58b37c682b
Kir322 <Fedulov.Kirill17@gmail.com>
2020-02-02 15:10:33 +0200

    indentation

--- init.el
+++ init.el
@@ -1,9 +1,9 @@
 (package-initialize)
 
 (setq package-archives
-      '(("gnu" . "http://elpa.gnu.org/packages/")
-	("marmalade" . "http://marmalade-repo.org/packages/")
-	("melpa" . "https://melpa.org/packages/")))
+  '(("gnu" . "http://elpa.gnu.org/packages/")
+   ("marmalade" . "http://marmalade-repo.org/packages/")
+   ("melpa" . "https://melpa.org/packages/")))
 
 (setq inhibit-startup-screen t)
 
@@ -19,18 +19,31 @@
 (global-superword-mode 1) ; treat snake_case as one word
 (delete-selection-mode 1) ; deletes selected region on typing
 
-(setq list-matching-lines-default-context-lines 1)
+(defun no-bell ())
+(setq ring-bell-function 'no-bell)
+
+(setq undo-limit 1000000000)
+(setq undo-strong-limit 2000000000)
+
+(setq indent-tabs-mode nil)
+
+(setq c-default-style "linux"
+      c-basic-offset 4
+      tab-width 4)
 
 (setq ido-enable-flex-matching t)
 (setq ido-everywhere t)
 (ido-mode 1)
 
+(clean-aindent-mode 1)
+
+(global-set-key (kbd "RET") 'newline-and-indent)
 (global-set-key (kbd "M-o") 'other-window)
 (global-set-key (kbd "M-i") 'imenu)
 
 (custom-set-variables
-  '(initial-frame-alist (quote ((fullscreen . maximized))))
-  '(package-selected-packages (quote (nord-theme))))
+ '(initial-frame-alist (quote ((fullscreen . maximized))))
+ '(package-selected-packages (quote (clean-aindent-mode nord-theme))))
 
 (add-to-list 'default-frame-alist '(font . "Monaco-14"))
 (set-face-attribute 'default t :font "Monaco-14")
